f671fffa48913bdb6641a83a43ba9c382933e5b8
Eduard Makarichak <makarichak1209@gmail.com>
2022-02-19 14:09:29 +0300

    finished brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = [];
+  let i = 0;
+  while (i < str.length) {
+      for (let config of bracketsConfig) {
+          if (str[i] == config[0] && config[0] == config[1] && result[result.length - 1] == config[0]) {
+            result.pop();
+              break;
+          }
+          if (str[i] == config[0]) {
+            result.push(str[i]);
+          }
+          if (str[i] == config[1] && config[0] != config[1]) {
+              if (result.length == 0) {
+                  return false;
+              }
+              if (result.length != 0 && result[result.length - 1] == config[0]) {
+                result.pop();
+              }
+          }
+          
+      }
+      i++;
+  }
+  if (result.length) {
+      return false;
+  }
+  return true;
 }
